875a58280aa8f621323c66218132c018614c7c68
DeK <nofatclips@gmail.com>
2012-12-30 18:20:33 +0100

    Optimized sieve: even numbers are not stored
    Signed-off-by: DeK

--- 20121230 - sieve/sieve.py
+++ 20121230 - sieve/sieve.py
@@ -11,48 +11,46 @@ class BitArray:
 
     @staticmethod
     def makeBitArray(bitSize, fill = 0):
-        intSize = bitSize >> 5                   # number of 32 bit integers
-        if (bitSize & 31):                      # if bitSize != (32 * n) add
-            intSize += 1                        #    a record for stragglers
-        if fill == 1:
-            fill = 4294967295                                 # all bits set
-        else:
-            fill = 0                                      # all bits cleared
+        intSize = bitSize >> 5   # number of 32 bit integers
 
-        bitArray = array.array('I')          # 'I' = unsigned 32-bit integer
+        # if bitSize != (32 * n) add a record for stragglers
+        if (bitSize & 31):                      
+            intSize += 1
+
+        if fill == 1: fill = 4294967295 # all bits set
+        else: fill = 0 # all bits cleared
+
+        bitArray = array.array('I') # 'I' = unsigned 32-bit integer
         bitArray.extend((fill,) * intSize)
         return(bitArray)
 
-    # testBit() returns a nonzero result, 2**offset, if the bit at 'bit_num' is set to 1.
+    # testBit() returns 1 if the bit at 'bit_num' is not set to 0.
     def testBit(array_name, bit_num):
         record = bit_num >> 5
         offset = bit_num & 31
         mask = 1 << offset
-        return(array_name.theArray[record] & mask)
+        return(array_name.theArray[record] & mask) != 0
 
-    # setBit() returns an integer with the bit at 'bit_num' set to 1.
+    # setBit() sets 'bit_num' to 1.
     def setBit(array_name, bit_num):
         record = bit_num >> 5
         offset = bit_num & 31
         mask = 1 << offset
         array_name.theArray[record] |= mask
-        return(array_name.theArray[record])
 
-    # clearBit() returns an integer with the bit at 'bit_num' cleared.
+    # clearBit() sets 'bit_num' to 0.
     def clearBit(array_name, bit_num):
         record = bit_num >> 5
         offset = bit_num & 31
         mask = ~(1 << offset)
         array_name.theArray[record] &= mask
-        return(array_name.theArray[record])
 
-    # toggleBit() returns an integer with the bit at 'bit_num' inverted, 0 -> 1 and 1 -> 0.
+    # toggleBit() inverts 'bit_num'
     def toggleBit(array_name, bit_num):
         record = bit_num >> 5
         offset = bit_num & 31
         mask = 1 << offset
         array_name.theArray[record] ^= mask
-        return(array_name[record])
 
 # My Code
 
@@ -62,8 +60,8 @@ class Sieve:
         self.maxNum = maxNum
         
         # Array of bits: 0 for non prime, 1 for prime
-        self._primes = BitArray(maxNum+1,1)
-        self.setNotPrime(0)
+        # Initialize at all primes except 1
+        self._primes = BitArray(maxNum//2+1,1)
         self.setNotPrime(1)
 
         # The prime number to be used for the next iteration
@@ -71,40 +69,59 @@ class Sieve:
         # range [1,nextIteration**2-1] and if you need to
         # evaluate a bigger number, you must first run more
         # iterations, until the bigger number is in the range
-        self.nextIteration = 2
+        # Starts at 3 because 2 is treated as a special case
+        # and the other even numbers are not even stored
+        self.nextIteration = 3
 
     def setPrime(self,n):
-        self._primes.setBit (n)
+        self._primes.setBit (Sieve._indexFor(n))
 
     def setNotPrime(self,n):
-        self._primes.clearBit (n)
+        self._primes.clearBit (Sieve._indexFor(n))
 
     def _isMarkedAsPrime(self,n):
-        return self._primes.testBit (n) != 0
+        '''True if n is marked with a '1' in the bit array'''
+        return self._primes.testBit (Sieve._indexFor(n)) != 0
+
+    @staticmethod
+    def _indexFor(n):
+        '''Even numbers are not stored, to save space.
+           Odd numbers are stored at half of their value'''
+        #print ("Index for %i is %i" % (n, n//2))
+        return n//2
 
     def _maxValid(self):
         return self.nextIteration**2-1
 
     def isPrime(self,n):
+        if n>self.maxNum:
+            raise IndexError("This sieve accepts values in [1,%i]" % (self.maxNum))
+        if n==2: return True
+        if not n%2: return False
+
+        # Do as many iterations of the sieve as necessary
+        # until we're sure that n is marked as prime if and
+        # only if it's actually a prime number
         while n>self._maxValid():
             self._nextIteration()
-        if n>self.maxNum: raise IndexError("This sieve accepts values in [1,%i]" % (self.maxNum))
         return self._isMarkedAsPrime(n)
 
     def _nextIteration(self):
         #print ("Iteration: %i" % (self.nextIteration) )
-        startAt = self._maxValid()+1
-        for i in range (startAt, self.maxNum+1, self.nextIteration):
+        startAt, stopAt, step = \
+            self._maxValid()+1, self.maxNum+1, self.nextIteration*2
+        for i in range (startAt, stopAt, step):
             #print ("Is not prime: %i" % (i) )
             self.setNotPrime(i)
         self._findNextPrime()
 
     def _findNextPrime(self):
-        self.nextIteration+=1
-        while (not self._isMarkedAsPrime(self.nextIteration)):
+        self.nextIteration+=2
+        while (not self.isPrime(self.nextIteration)):
             self.nextIteration+=1
 
-sieve = Sieve(100)
-for i in range(100):
-    if sieve.isPrime(i):
-        print(i)
+    @staticmethod
+    def checkPrime(n):
+        return Sieve(n).isPrime(n)
+
+print (Sieve.checkPrime(1009))
