9d71d50abe5d784b410a7d6f22c4e9f3bc62e22a
DeK <nofatclips@gmail.com>
2013-01-17 12:50:26 +0100

    Separation of concerns: DisassemblyLine is responsible for the data, the other class for the algorithm
    Signed-off-by: DeK

--- 201301 - substrings/substrings.py
+++ 201301 - substrings/substrings.py
@@ -1,28 +1,62 @@
 from collections import deque
 
+class DisassemblyLine:
+
+    def __init__ (self):
+        self._set = set()
+        self._queue = deque()
+
+    def push (self, value):
+        #print ("Pushing " + value)
+        self._set.add(value)
+        self._queue.append(value)
+
+    def pop (self):
+        #print ("Popping")
+        return self._queue.popleft()
+
+    def __bool__ (self):
+        #print ("Checking non zero")
+        return True if self._queue else False
+
+    def __len__ (self):
+        #print ("Len")
+        return len(self._set)
+
+    def __call__ (self):
+        return self._set
+
 class SubstringGenerator:
 
     def __init__(self, word):
         self.word = word
-        self._subList = set()
-        self.count()
+        self._subList = self.count()
+        #self.count()
 
     def count(self):
-        theList, strings = self._subList, deque()
-        theList.add(self.word)
-        strings.append(self.word)
+        #theList, strings = set(), deque()
+        counter = DisassemblyLine()
+        #theList.add(self.word)
+        #strings.append(self.word)
+        counter.push(self.word)
 
         l=0
-        while strings:
-            string = strings.popleft()
+        #while strings:
+        while counter:
+            #string = strings.popleft()
+            string = counter.pop()
             l, previousL = len(string),l
             lWord, rWord = string[:-1], string[1:]
             if l!=previousL and lWord:
-                theList.add(lWord)
-                strings.append(lWord)
+                #theList.add(lWord)
+                #strings.append(lWord)
+                counter.push(lWord)
             if rWord:
-                theList.add(rWord)
-                strings.append(rWord)            
+                #theList.add(rWord)
+                #strings.append(rWord)
+                counter.push(rWord)
+
+        return counter()
 
     # Delegate protocols to the subList object
 
